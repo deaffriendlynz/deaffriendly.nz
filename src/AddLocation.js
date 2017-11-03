33339c1b4e3dac9e385d4471b075ecb2f4da72af
Joshua Vial <joshua@enspiral.com>
2017-11-04 11:10:45 +1300

    placeholder components

---
+++ src/AddLocation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AddLocation extends Component {
+  render() {
+    return (
+      <div className='AddLocation'>
+      </div>
+    )
+  }
+}
+
+export default AddLocation
---
+++ src/LocationMap.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LocationMap extends Component {
+  render() {
+    return (
+      <div className='LocationMap'>
+      </div>
+    )
+  }
+}
+
+export default LocationMap
