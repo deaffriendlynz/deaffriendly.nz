d61a3007f2798406c26ea5f153f96db7db7670a3
Joshua Vial <joshua@enspiral.com>
2017-11-05 10:03:59 +1300

    new logo

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import AddLocation from './AddLocation'
 import LocationMap from './LocationMap'
 import './App.css';
+import logo from './nzsl-friendly-logo.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Deaf Friendly NZ</h1>
-          <AddLocation />
-          <LocationMap />
+          <img src={logo} alt='Deaf Friendly NZ' />
         </header>
         <p className="App-intro">
-          An online travel guide by and for the deaf community.
+
         </p>
+        <AddLocation />
+        <LocationMap />
       </div>
     );
   }
